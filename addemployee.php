88c3201166e2c9893bdaee435a7fe1aab00451b0
tobiloba123 <godsownlove777@gmail.com>
2019-07-13 08:19:59 +0000

    Add files via upload

---
+++ addemployee.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>CHED Contribution System| Dashboard</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  
+  <link rel="stylesheet" href="dist/css/skins/skin-blue.min.css">
+
+  <!-- Google Font -->
+  <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <!-- Main Header -->
+  <header class="main-header">
+
+    <!-- Logo -->
+    <a href="dashboard.php" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>CH</b>ED</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>CHED</b>Contribution</span>
+    </a>
+
+    <!-- Header Navbar -->
+    <nav class="navbar navbar-static-top" role="navigation">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+      <!-- Navbar Right Menu -->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <!-- Messages: style can be found in dropdown.less-->
+          
+          <!-- /.messages-menu -->
+
+          <!-- Notifications Menu -->
+          
+          <!-- Tasks Menu -->
+          
+          <!-- User Account Menu -->
+          <li class="dropdown user user-menu">
+            <!-- Menu Toggle Button -->
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <!-- The user image in the navbar-->
+              <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <!-- hidden-xs hides the username on small devices so only the image appears. -->
+              <span class="hidden-xs">Alexander Pierce</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- The user image in the menu -->
+              <li class="user-header">
+                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
+                <p>
+                  Alexander Pierce - Web Developer
+                  <small>Member since Nov. 2012</small>
+                </p>
+              </li>
+              <!-- Menu Body -->
+              
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                </div>
+                
+                <div class="pull-right">
+                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                </div>
+              </li>
+
+              <li class="user-footer">
+                
+                <div class="pull-right">
+                  <a href="#" class="btn btn-default btn-flat">Change Password</a>
+                </div>
+                
+              </li>
+            </ul>
+          </li>
+          <!-- Control Sidebar Toggle Button -->
+          <!-- <li>
+            <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+          </li> -->
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Alexander Pierce</p>
+          <!-- Status -->
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+
+      <!-- search form (Optional) -->
+     
+      <!-- /.search form -->
+
+      <!-- Sidebar Menu -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MENU</li>
+        <!-- Optionally, you can add icons to the links -->
+        <li class="active"><a href="dashboard.php"><i class="fa fa-home"></i> <span>Home</span></a></li>
+        <li><a href="department.php"><i class="fa fa-building"></i> <span>Departments</span></a></li>
+        <li><a href="employees.php"><i class="fa fa-users"></i> <span>Employees</span></a></li>
+        <li><a href="contributiontypes.php"><i class="fa fa-tags"></i> <span>Contribution Types</span></a></li>
+        <li><a href="makecontribution.php"><i class="fa fa-bars"></i> <span>Make Contribution</span></a></li>
+        <li><a href="contributions.php"><i class="fa fa-tag"></i> <span>Contributions</span></a></li>
+        <li><a href="withrequest.php"><i class="fa fa-registered"></i> <span>Withdrawal Request</span></a></li>
+        <li><a href="withdrawals.php"><i class="fa fa-list"></i> <span>Withdrawals</span></a></li>
+        <li><a href="reports.php"><i class="fa fa-folder"></i> <span>Reports</span></a></li>
+        <li class="header">others</li>
+        <li><a href="import.php"><i class="fa fa-upload"></i> <span>Import</span></a></li>
+        <!-- <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Link in level 2</a></li>
+            <li><a href="#">Link in level 2</a></li>
+          </ul>
+        </li> -->
+      </ul>
+      <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Employee Form
+        <small>Create Employee</small>
+      </h1>
+    </section>
+
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <!-- left column -->
+        <div class="col-md-8 ml-2">
+          <!-- general form elements -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Fill the form Below</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form">
+              <div class="box-body">
+               <!--  <div class="row">
+                  <div class="col-md-6"> -->
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Staff ID</label>
+                      <input type="number" class="form-control" id="exampleInputEmail1" placeholder="Enter Staff ID">
+                    </div>
+              <!--     </div>
+                </div> -->
+                
+                <div class="form-group">
+                  <label for="exampleInputEmail1">FullName</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter Employees Fullname">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email Address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter Email">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Phone Number</label>
+                  <input type="number" class="form-control" id="exampleInputEmail1" placeholder="Enter Phone Number">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Salary</label>
+                  <input type="number" class="form-control" id="exampleInputEmail1" placeholder="Enter Staff Salary">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputFile">Profile Picture</label>
+                  <input type="file" id="exampleInputFile">
+
+                  <p class="help-block">Example block-level help text here.</p>
+                </div>
+                
+                <div class="form-group">
+                  <label>Select Department</label>
+                  <select class="form-control">
+                    <option>option 1</option>
+                    <option>option 2</option>
+                    <option>option 3</option>
+                    <option>option 4</option>
+                    <option>option 5</option>
+                  </select>
+                </div>
+
+                   <div class="form-group">
+                  <label>Select User Role</label>
+                  <select class="form-control">
+                    <option>option 1</option>
+                    <option>option 2</option>
+                    <option>option 3</option>
+                    <option>option 4</option>
+                    <option>option 5</option>
+                  </select>
+                </div>
+                
+                
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
+
+
+
+
+
+
+
+
+
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Version 2.2.4
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2019 <a href="#">CHED</a>.</strong> All rights reserved.
+  </footer>
+
+  
+  <div class="control-sidebar-bg"></div>
+</div>
+
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+
+
+</body>
+</html>
---
+++ reports.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>CHED Contribution System| Dashboard</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  
+  <link rel="stylesheet" href="dist/css/skins/skin-blue.min.css">
+
+  <!-- Google Font -->
+  <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <!-- Main Header -->
+  <header class="main-header">
+
+    <!-- Logo -->
+    <a href="dashboard.php" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>CH</b>ED</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>CHED</b>Contribution</span>
+    </a>
+
+    <!-- Header Navbar -->
+    <nav class="navbar navbar-static-top" role="navigation">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+      <!-- Navbar Right Menu -->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <!-- Messages: style can be found in dropdown.less-->
+          
+          <!-- /.messages-menu -->
+
+          <!-- Notifications Menu -->
+          
+          <!-- Tasks Menu -->
+          
+          <!-- User Account Menu -->
+          <li class="dropdown user user-menu">
+            <!-- Menu Toggle Button -->
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <!-- The user image in the navbar-->
+              <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <!-- hidden-xs hides the username on small devices so only the image appears. -->
+              <span class="hidden-xs">Alexander Pierce</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- The user image in the menu -->
+              <li class="user-header">
+                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
+                <p>
+                  Alexander Pierce - Web Developer
+                  <small>Member since Nov. 2012</small>
+                </p>
+              </li>
+              <!-- Menu Body -->
+              
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                </div>
+                
+                <div class="pull-right">
+                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                </div>
+              </li>
+
+              <li class="user-footer">
+                
+                <div class="pull-right">
+                  <a href="#" class="btn btn-default btn-flat">Change Password</a>
+                </div>
+                
+              </li>
+            </ul>
+          </li>
+          <!-- Control Sidebar Toggle Button -->
+          <!-- <li>
+            <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+          </li> -->
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Alexander Pierce</p>
+          <!-- Status -->
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+
+      <!-- search form (Optional) -->
+     
+      <!-- /.search form -->
+
+      <!-- Sidebar Menu -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MENU</li>
+        <!-- Optionally, you can add icons to the links -->
+        <li class="active"><a href="dashboard.php"><i class="fa fa-home"></i> <span>Home</span></a></li>
+        <li><a href="department.php"><i class="fa fa-building"></i> <span>Departments</span></a></li>
+        <li><a href="employees.php"><i class="fa fa-users"></i> <span>Employees</span></a></li>
+        <li><a href="contributiontypes.php"><i class="fa fa-tags"></i> <span>Contribution Types</span></a></li>
+        <li><a href="makecontribution.php"><i class="fa fa-bars"></i> <span>Make Contribution</span></a></li>
+        <li><a href="contributions.php"><i class="fa fa-tag"></i> <span>Contributions</span></a></li>
+        <li><a href="withrequest.php"><i class="fa fa-registered"></i> <span>Withdrawal Request</span></a></li>
+        <li><a href="withdrawals.php"><i class="fa fa-list"></i> <span>Withdrawals</span></a></li>
+        <li><a href="reports.php"><i class="fa fa-folder"></i> <span>Reports</span></a></li>
+        <li class="header">others</li>
+        <li><a href="import.php"><i class="fa fa-upload"></i> <span>Import</span></a></li>
+        <!-- <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Link in level 2</a></li>
+            <li><a href="#">Link in level 2</a></li>
+          </ul>
+        </li> -->
+      </ul>
+      <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        <!-- Make Contribution Page -->
+      </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+      <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+
+          <div style="margin-top: 90px;">
+            <h1 style="text-align: center;"> Please Select Report Type </h1>
+            <br>
+            <br>
+          </div>
+        <div class="row">
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <a href="withdrawal_report.php" class="small-box-footer">
+            <div class="inner">
+              <h3>Withdrawals Report</h3>
+
+              <!-- <p>Single Transaction allows you to make contribution for an employee.</p> -->
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            Click Here <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <a href="#" class="small-box-footer">
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>Contributions Report</h3>
+
+              <!-- <p>Upload an Excel File Type for data importation</p> -->
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            More info <i class="fa fa-upload"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <a href="employee_contribution_report.php" class="small-box-footer">
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>Employee Contribution</h3>
+
+              <!-- <p>Upload an Excel File Type for data importation</p> -->
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            More info <i class="fa fa-upload"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <a href="#" class="small-box-footer">
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>Annual Report</h3>
+
+              <!-- <p>Upload an Excel File Type for data importation</p> -->
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            More info <i class="fa fa-upload"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        
+        <!-- ./col -->
+      </div>
+      <!-- /.row -->
+      <!-- Main row -->
+       
+     
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Version 2.2.4
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2019 <a href="#">CHED</a>.</strong> All rights reserved.
+  </footer>
+
+  
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- DataTables -->
+<script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+
+<script>
+  $(function () {
+    $('#example1').DataTable()
+    $('#example2').DataTable({
+      'paging'      : true,
+      'lengthChange': false,
+      'searching'   : false,
+      'ordering'    : true,
+      'info'        : true,
+      'autoWidth'   : false
+    })
+  })
+</script>
+</body>
+</html>
